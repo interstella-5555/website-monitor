afd154e89853dff535bfaf50156bc92454d81b0f
Interstella 5555 <3755029+interstella-5555@users.noreply.github.com>
2026-03-09 21:58:09 +0100

    Default monitors to disabled, require MONITOR_X=true to enable

--- src/monitors/index.ts
+++ src/monitors/index.ts
@@ -6,5 +6,5 @@ const all = [bookeroMonitor, ghibliMonitor, sportivoMonitor];
 
 export const monitors = all.filter((m) => {
   const envKey = `MONITOR_${m.name.toUpperCase()}`;
-  return process.env[envKey] !== "false";
+  return process.env[envKey] === "true";
 });
